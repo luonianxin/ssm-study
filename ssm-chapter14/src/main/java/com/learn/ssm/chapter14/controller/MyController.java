77cb954408f23430abb3d102c3b2bd4165f677a2
luonianxin <1423364324@qq.com>
2020-09-30 14:27:42 +0800

    由于ｐｏｍ配置文件导致的编译与部署失败问题，修改了ｍａｖｅｎ的ｐｏｍ配置文件

--- ssm-chapter14/src/main/java/com/learn/ssm/chapter14/controller/MyController.java
+++ ssm-chapter14/src/main/java/com/learn/ssm/chapter14/controller/MyController.java
@@ -1,4 +1,17 @@
 package com.learn.ssm.chapter14.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("myController")
+@RequestMapping("/my")
 public class MyController {
+
+    @RequestMapping("/index")
+    public ModelAndView index(){
+        ModelAndView mv  = new ModelAndView();
+        mv.setViewName("index");
+        return mv;
+    }
 }
